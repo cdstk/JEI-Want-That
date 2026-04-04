19cd827ecb95c45e28ffada8bc39b3822963223c
cdstk <cdstkemail@gmail.com>
2026-04-03 23:48:11 -0500

    1.0.2

--- src/main/java/jeiwt/JEIWantThat.java
+++ src/main/java/jeiwt/JEIWantThat.java
@@ -30,7 +30,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class JEIWantThat {
     public static final String MODID = "jeiwt";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final String NAME = "JEI Want That";
     public static final Logger LOGGER = LogManager.getLogger();
     public static boolean completedLoading = false;
