b4c76821d6ad77aaf664b403a79d3b41f3d4327d
cdstk <cdstkemail@gmail.com>
2026-08-03 23:56:03 -0500

    Bump 1.0.3.5

--- src/main/java/jeiwt/JEIWantThat.java
+++ src/main/java/jeiwt/JEIWantThat.java
@@ -38,7 +38,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class JEIWantThat {
     public static final String MODID = "jeiwt";
-    public static final String VERSION = "1.0.3.4";
+    public static final String VERSION = "1.0.3.5";
     public static final String NAME = "JEI Want That";
     public static final Logger LOGGER = LogManager.getLogger();
     public static boolean completedLoading = false;
